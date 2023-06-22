f7ea58aca43396c619759200b2752b34b4f9b3bf
Suman Sourabh <sumansourabh9155@gmail.com>
2023-06-22 22:45:28 +0530

    Changed Navber shadow and position

--- src/resource/component/Navbar.jsx
+++ src/resource/component/Navbar.jsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import ReactGA from "react-ga";
-ReactGA.initialize("G-R2Q7XQ6WBW");
-ReactGA.pageview(window.location.pathname + window.location.search);
+
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    ReactGA.initialize("G-R2Q7XQ6WBW");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -31,11 +34,11 @@ const Navbar = () => {
   return (
     <div>
       <div
-        className={`fixed w-full transition-transform duration-300  ${
-          isScrolled ? "translate-y-0" : "-translate-y-20"
+        className={`fixed w-full transition-transform duration-300 ${
+          isScrolled ? "translate-y-0" : "-translate-y-24"
         }`}
       >
-        <header className="lg:pr-28 w-full text-gray-700 shadow-sm body-font fixed item-center justify-between bg-white">
+        <header className="lg:px-0 w-[95%] text-gray-700 shadow-sm bottom body-font fixed item-center justify-between bg-white">
           <div className="container flex flex-col p-6 mx-auto md:flex-row justify-between">
             <div className="flex gap-40">
               <Link
@@ -48,7 +51,7 @@ const Navbar = () => {
               </Link>
               <div className="md:hidden">
                 <button
-                  className="flex items-center justify-center p-2 text-gray-900 transition-colors duration-200 rounded focus:outline-none hover:bg-gray-200 "
+                  className="flex items-center justify-center p-2 text-gray-900 transition-colors duration-200 rounded focus:outline-none hover:bg-gray-200"
                   onClick={toggleMenu}
                 >
                   <svg
@@ -70,82 +73,116 @@ const Navbar = () => {
             </div>
             <nav className="hidden md:flex md:flex-wrap items-center justify-between text-base md:ml-auto md:mr-auto">
               <NavLink
-                
                 to="/projects"
                 className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "mr-5 font-bold  text-[#e76e50]" : "mr-5 block font-medium hover:text-gray-700"
-                  }
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "mr-5 font-bold text-[#e76e50]"
+                    : "mr-5 block font-medium hover:text-gray-700"
+                }
               >
                 Project
               </NavLink>
               <NavLink
                 to="/resources"
                 className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "mr-5 font-bold text-[#e76e50]" : "mr-5 block font-medium hover:text-gray-900"
-                  }
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "mr-5 font-bold text-[#e76e50]"
+                    : "mr-5 block font-medium hover:text-gray-900"
+                }
               >
                 Resources
               </NavLink>
-              <NavLink to="/Blog" className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "mr-5 font-bold text-[#e76e50]" : "mr-5 block font-medium hover:text-gray-700"
-                  }>
+              <NavLink
+                to="/blog"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "mr-5 font-bold text-[#e76e50]"
+                    : "mr-5 block font-medium hover:text-gray-700"
+                }
+              >
                 Blog
               </NavLink>
-              <NavLink to="/About" className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "font-bold text-[#e76e50]" : " block font-medium hover:text-gray-700"
-                  }>
+              <NavLink
+                to="/about"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "font-bold text-[#e76e50]"
+                    : " block font-medium hover:text-gray-700"
+                }
+              >
                 About Me
               </NavLink>
             </nav>
 
-            {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-            <div className={`md:hidden  ${isMenuOpen ? "block" : "hidden"}`}>
+            {/* Mobile menu, show/hide based on menu state */}
+            <div className={`md:hidden ${isMenuOpen ? "block" : "hidden"}`}>
               <div className="flex flex-col items-center pt-2 pb-4 space-y-1">
                 <NavLink
                   to="/projects"
                   className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "font-bold text-[#e76e50]" : "block font-medium hover:text-gray-700"
-                }
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "font-bold text-[#e76e50]"
+                      : "block font-medium hover:text-gray-700"
+                  }
                 >
                   Project
                 </NavLink>
                 <NavLink
                   to="/resources"
                   className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "font-bold text-[#e76e50]" : "block font-medium hover:text-gray-700"
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "font-bold text-[#e76e50]"
+                      : "block font-medium hover:text-gray-700"
                   }
                 >
                   Resources
                 </NavLink>
                 <NavLink
-                  to="/Blog"
+                  to="/blog"
                   className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "font-bold text-[#e76e50]" : "block font-medium hover:text-gray-700"
-                }
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "font-bold text-[#e76e50]"
+                      : "block font-medium hover:text-gray-700"
+                  }
                 >
                   Blog
                 </NavLink>
                 <NavLink
                   to="/about"
                   className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "font-bold text-[#e76e50]" : "block font-medium hover:text-gray-700"
-                }
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "font-bold text-[#e76e50]"
+                      : "block font-medium hover:text-gray-700"
+                  }
                 >
                   About Me
                 </NavLink>
                 <NavLink
                   to="/contact"
-                  className="px-4 py-2 text-xs font-bold text-white uppercase bg-[#e76e50] rounded shadow outline-none  hover:shadow-md focus:outline-none ease transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700 duration-300 ..."
+                  className="px-4 py-2 text-xs font-bold text-white uppercase bg-[#e76e50] rounded shadow outline-none hover:shadow-md focus:outline-none ease transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700 duration-300 ..."
                 >
                   Contact
                 </NavLink>
               </div>
             </div>
             <div className="items-center hidden md:flex h-full">
-              <Link
-                to="/contact"
-                className="buttonsmall " role="button"
-              >
+              <Link to="/contact" className="buttonsmall" role="button">
                 Contact
               </Link>
             </div>
